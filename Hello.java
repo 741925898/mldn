facb96d25ca673ced7d9970dcf9db77e287a00cb
qianxinghui <741925898@qq.com>
2018-10-04 18:02:56 +0800

    simple print

--- Hello.java
+++ Hello.java
@@ -1,12 +1,6 @@
 public class Hello{
-	public static final String COMPANY_NAME="admin";
 	public static void main(){
-		system.out.println("Happy New Year");
-		system.out.println("Happy Myself");
-	}
-	public static void print(){
-		system.out.println("*****************");
-		system.out.println("Hello world!!");
-		system.out.println("*****************");
+		system.out.println("****************");
+		system.out.println("###########");
 	}
 }
