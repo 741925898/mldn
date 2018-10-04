5c11b70a0b8b5e55c955aeaad2458c96eea342cb
qianxinghui <741925898@qq.com>
2018-10-04 08:49:06 +0800

    add Demo.java

---
+++ Demo.java
@@ -0,0 +1,5 @@
+public class Demo{
+	public static void main(){
+		system.out.println("Hello ADMIN !");
+	}
+}
--- Hello.java
+++ Hello.java
@@ -1,6 +1,5 @@
 public class Hello{
 	public static void main(){
-		system.out.println("Hello ADMIN !");
 		system.out.println("www.baidu.com");
 	}
 }
