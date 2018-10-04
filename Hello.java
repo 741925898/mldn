972e14c8c7581d4a657374e637069dfb9a426d00
qianxinghui <741925898@qq.com>
2018-10-04 17:57:56 +0800

    add static method

--- Hello.java
+++ Hello.java
@@ -3,4 +3,9 @@ public class Hello{
 		system.out.println("Happy New Year");
 		system.out.println("Happy Myself");
 	}
+	public static void print(){
+		system.out.println("*****************");
+		system.out.println("Hello world!!");
+		system.out.println("*****************");
+	}
 }
